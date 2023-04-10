ef22f72ac926a303175724fc8f1f0fd8833d6f44
juliochavezpro <juliochavezpro@gmail.com>
2023-04-10 16:31:19 -0400

    Add UI

---
+++ script.js
@@ -0,0 +1,9 @@
+const buttons = document.getElementById('.buttons');
+
+const playingPress = buttons.addEventListener('keydown', function(e) {
+
+});
+
+const playingClick = buttons.addEventListener('click', function(e) {
+    buttons
+});
